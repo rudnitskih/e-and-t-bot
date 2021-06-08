9b960e7126db2f2a5db6974d58665a39a2763b0f
Dmytro Rudnytskykh <rudnitskih@gmail.com>
2021-06-08 22:39:53 +0300

    Add telegram support

--- index.js
+++ index.js
@@ -1,14 +1,34 @@
-
 const http = require('http');
+const {Telegraf, Markup} = require('telegraf');
 
 const PORT = process.env.PORT || 5000;
+const bot = new Telegraf(process.env.BOT_TOKEN);
+const EventDetails = {
+  EMOTION: '🎭 Эмоция',
+  EMOTION_RANK: '🔢 Сила эмоции'
+}
+// const secretPath = `/telegraf/${bot.secretPathComponent()}`
 
-const server = http.createServer((req, res) => {
-  res.statusCode = 200;
-  res.setHeader('Content-Type', 'text/plain');
-  res.end('Hello World');
+const eventKeyboard = Markup.keyboard([[EventDetails.EMOTION, EventDetails.EMOTION_RANK]]);
+
+bot.start((ctx) => ctx.reply('Welcome'));
+bot.help((ctx) => ctx.reply('Send me a sticker'));
+bot.on('text', async (ctx) => {
+  console.log('Ctx=', ctx);
+  // Using context shortcut
+  await ctx.reply('Event added to notion', eventKeyboard);
 });
 
-server.listen(PORT, () => {
+bot.launch();
+
+
+// Enable graceful stop
+process.once('SIGINT', () => bot.stop('SIGINT'));
+process.once('SIGTERM', () => bot.stop('SIGTERM'));
+
+http.createServer(() => {
+  // bot.webhookCallback(secretPath);
+
+}).listen(PORT, () => {
   console.log(`Server running at ${PORT}`);
 });
