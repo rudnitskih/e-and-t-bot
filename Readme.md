7223921408dccf8c23865d46b1d9675b0f3c7fc2
Dmytro Rudnytskykh <rudnitskih@gmail.com>
2021-06-08 21:26:36 +0300

    Init project

---
+++ Readme.md
@@ -0,0 +1,3 @@
+# Emotions and Thoughts 
+
+Simple telegram bot which gather emotions and thoughts and store them in Notion 
---
+++ index.js
@@ -0,0 +1,15 @@
+
+const http = require('http');
+
+const hostname = '127.0.0.1';
+const PORT = process.env.PORT || 5000;
+
+const server = http.createServer((req, res) => {
+  res.statusCode = 200;
+  res.setHeader('Content-Type', 'text/plain');
+  res.end('Hello World');
+});
+
+server.listen(PORT, hostname, () => {
+  console.log(`Server running at http://${hostname}:${PORT}/`);
+});
