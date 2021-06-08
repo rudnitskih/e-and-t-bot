41d4da3101c1b5e76947214b19ad1d319adaa7a5
Dmytro Rudnytskykh <rudnitskih@gmail.com>
2021-06-08 21:35:01 +0300

    Listen port on all hosts

--- index.js
+++ index.js
@@ -1,7 +1,6 @@
 
 const http = require('http');
 
-const hostname = '127.0.0.1';
 const PORT = process.env.PORT || 5000;
 
 const server = http.createServer((req, res) => {
@@ -10,6 +9,6 @@ const server = http.createServer((req, res) => {
   res.end('Hello World');
 });
 
-server.listen(PORT, hostname, () => {
-  console.log(`Server running at http://${hostname}:${PORT}/`);
+server.listen(PORT, () => {
+  console.log(`Server running at ${PORT}`);
 });
